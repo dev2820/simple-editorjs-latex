fc5d7dec50eb36dd5dc0c46c6318c417fdabf385
dev2820 <second28200@gmail.com>
2022-11-08 13:00:00 +0900

    chore/ webpack dev server config

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,17 @@
-export default {
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
   entry: "./src/index.js",
   devServer: {
     static: "./dist",
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Development",
+      template: "example/index.html",
+    }),
+  ],
   optimization: {
     runtimeChunk: "single",
   },
