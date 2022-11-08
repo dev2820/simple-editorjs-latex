f2ef5a16573998c2d6b00c1df427fef62dd6715a
dev2820 <second28200@gmail.com>
2022-11-08 16:48:20 +0900

    feature/ show placeholder when block is empty

--- src/constants/PREVIEW.js
+++ src/constants/PREVIEW.js
@@ -1,5 +1,6 @@
 export default {
   SELECTOR: {
     SELF: "latex-preview",
+    EMPTY: "empty",
   },
 };
--- src/ui/createPreview.js
+++ src/ui/createPreview.js
@@ -4,7 +4,14 @@ const createInput = () => {
   const $preview = document.createElement("div");
 
   $preview.classList.add(PREVIEW.SELECTOR.SELF);
-
+  $preview.classList.add(PREVIEW.SELECTOR.EMPTY);
+  const observer = new MutationObserver(() => {
+    const isEmpty = $preview.innerText === "";
+    $preview.classList.toggle("empty", isEmpty);
+  });
+  observer.observe($preview, {
+    childList: true,
+  });
   return $preview;
 };
 
