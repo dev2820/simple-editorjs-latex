99fe149d4981c1c3cae51ce3534c824da11e856d
dev2820 <second28200@gmail.com>
2022-11-08 17:00:21 +0900

    chore/ webpack config except about example

--- webpack.config.js
+++ webpack.config.js
@@ -5,38 +5,12 @@ module.exports = {
   mode: "development",
   entry: "./src/index.js",
   target: "web",
-<<<<<<< HEAD
   output: {
     filename: "LaTeX.js",
     path: path.resolve(__dirname, "dist"),
     library: "editorjs-latex",
     libraryTarget: "umd",
     libraryExport: "default",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-      },
-    ],
-=======
-  devServer: {
-    static: "./dist",
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "Development",
-      template: "example/index.html",
-    }),
-  ],
-  output: {
-    filename: "LaTeX.js",
-    path: path.resolve(__dirname, "dist"),
-    library: "LaTeX",
-    libraryTarget: "umd",
-    libraryExport: "default",
->>>>>>> 31a0904 (chore/ package build option change (to create LaTeX file))
   },
   module: {
     rules: [
