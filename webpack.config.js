ff6eba59e29c36b914ac97802f5a15aef407b203
dev2820 <second28200@gmail.com>
2022-11-08 14:38:37 +0900

    chore/ package build option change (to create LaTeX file)

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
 
 module.exports = {
   mode: "development",
   entry: "./src/index.js",
   target: "web",
+<<<<<<< HEAD
   output: {
     filename: "LaTeX.js",
     path: path.resolve(__dirname, "dist"),
@@ -18,5 +20,22 @@ module.exports = {
         use: ["style-loader", "css-loader"],
       },
     ],
+=======
+  devServer: {
+    static: "./dist",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Development",
+      template: "example/index.html",
+    }),
+  ],
+  output: {
+    filename: "LaTeX.js",
+    path: path.resolve(__dirname, "dist"),
+    library: "LaTeX",
+    libraryTarget: "umd",
+    libraryExport: "default",
+>>>>>>> 31a0904 (chore/ package build option change (to create LaTeX file))
   },
 };
